21b8d4a5040a5c1f028793c45666bc5db8984670
Logan McPhail <logan.airnomad@gmail.com>
2018-01-25 02:03:33 -0700

    trying conduit, still WIP but it's working

--- src/Process.hs
+++ src/Process.hs
@@ -1,40 +1,45 @@
 module Process where
 
-import           System.Exit          (ExitCode (..))
-import           System.IO            (hClose, hFlush, hGetLine, hIsEOF,
-                                       hPutStrLn)
-import           System.Process.Typed (closed, createPipe, getExitCode,
-                                       getStderr, getStdin, getStdout,
-                                       setStderr, setStdin, setStdout, shell,
-                                       withProcess)
+import           Control.Concurrent
+import           Control.Concurrent.Async (Concurrently (..))
+import qualified Data.Text                as T
+
+import           Data.Conduit
+import qualified Data.Conduit.Binary      as CB
+import qualified Data.Conduit.List        as CL
+import           Data.Conduit.Process     (ClosedStream (..), shell,
+                                           streamingProcess,
+                                           waitForStreamingProcess)
+import qualified Data.Conduit.Text        as CT
+import           GHC.IO.Exception         (ExitCode (..))
+
+data Msg = Quit | Msg T.Text | Error T.Text
+
+-- run :: Chan Msg -> Int -> String -> IO ()
+-- run chan i cmd = do
 
 run :: String -> IO ()
 run cmd = do
-  let config = setStdin createPipe
-              $ setStdout createPipe
-              $ setStderr createPipe
-              $ shell cmd
+  (ClosedStream, fromProcess, fromProcessErr, p) <- streamingProcess (shell cmd)
 
-  withProcess config $ \p -> do
-    let loop = do
-          isLogging   <- not <$> hIsEOF (getStdout p)
-          isErroring  <- not <$> hIsEOF (getStderr p)
-          if isLogging then do
-            out <- hGetLine $ getStdout p
-            print out
-            loop
-          else if isErroring then do
-            err <- hGetLine $ getStderr p
-            print err
-            loop
-          else do
-            c <- getExitCode p
-            maybe loop handleExit c
-    loop
+  let output h action =
+        CB.sourceHandle h $$  -- create conduit source from the handle (i.e. stdout, stderr)
+        CT.decode CT.utf8 =$= -- decode the bytestring as utf8 into Text
+        CT.lines =$=          -- split the text into a stream of lines
+        CL.mapM_ action       -- map the stream over the action
+        -- CL.mapM_ (writeChan chan . Msg)
 
+  let stdout = output fromProcess (putStrLn . T.unpack)
+  let stderr = output fromProcessErr (putStrLn .  (\s -> "\ESC[35;1m" ++ s ++ "\ESC[0m\n") . T.unpack)
 
-handleExit :: ExitCode -> IO ()
-handleExit code =
+  code <- runConcurrently $ -- run all three conduits, and capture exit code.
+    Concurrently stdout *>
+    Concurrently stderr *>
+    Concurrently (waitForStreamingProcess p)
+
   case code of
-    ExitSuccess   -> putStrLn "Success!"
-    ExitFailure i -> putStrLn $ "Failure! :( " ++ (show i)
+    ExitFailure x -> putStrLn "failure! D:"
+    ExitSuccess   -> putStrLn "success! :)"
+
+  putStrLn "exit"
+  -- writeChan chan Quit
